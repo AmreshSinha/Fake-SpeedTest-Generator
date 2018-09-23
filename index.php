73518b3e60d8be3c3ff969c4fe60e352f10190f5
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2018-09-23 15:57:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,48 @@
-<HTML>
+<!DOCTYPE html>
+<html>
 <head><title>Fake SpeedTest Gen-Amresh</title>
+
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+.active {
+    background-color: #4CAF50;
+}
+</style>
 </head>
 <body>
+
+<ul>
+  <li><a class="active" href="" onclick="window.location.reload()">Home</a></li>
+  <li><a target=_blank href="https://post4vps.com">Post4VPS</a></li>
+  <li><a target=_blank href="https://post4vps.com/user-1044.html">Contact</a></li>
+  <li><a target=_blank href="https://github.com/AmreshSinha/Fake-SpeedTest-Generator">About</a></li>
+</ul>
+
+
 <?php
 if(!isset($_POST['sub'])){
   echo '
