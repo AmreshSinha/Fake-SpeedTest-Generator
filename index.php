54ffb5df465ac637291a93a0aed50253825cacb3
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2021-09-15 21:18:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head><title>Fake SpeedTest Gen-Amresh</title>
 
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon">
 <!-- <link rel="stylesheet" href="./assets/css/style.css"> -->
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
